3ecc02f334c5c104abf347ec5734db0406ea4fb1
Christian Sandoval <christiansandoval1018@outlook.com>
2023-05-23 17:40:17 -0500

    progressed styling custom radio buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,51 @@
-import './App.scss';
-import { FaRegCopy } from 'react-icons/fa';
+import "./App.scss";
+import { FaRegCopy } from "react-icons/fa";
 
 function App() {
-	return (
-		<div className='App pg-app'>
-			<h1>Password Generator</h1>
-			<form action='' className='pg-form'>
-				<div className='input-wrapper'>
-					<input
-						type='text'
-						className='password'
-					/>
-					<div className='icon-wrapper'>
-						<FaRegCopy className='icon' />
-					</div>
-				</div>
-				<div className='settings-wrapper'>
-					<div className='row-space-between'>
-						<h3>Character Length</h3>
-						<span className='count'>0</span>
-					</div>
+  return (
+    <div className="App pg-app">
+      <h1>Password Generator</h1>
+      <form action="" className="pg-form">
+        <div className="input-wrapper">
+          <input type="text" className="password" />
+          <div className="icon-wrapper">
+            <FaRegCopy className="icon" />
+          </div>
+        </div>
+        <div className="settings-wrapper">
+          <div className="row-space-between">
+            <h3>Character Length</h3>
+            <span className="count">0</span>
+          </div>
 
-					<div className='slider-length'>
-						<div className='circle'></div>
-					</div>
+          <div className="slider-length">
+            <div className="circle"></div>
+          </div>
 
-                    <label className="container">One
-                    <input type="checkbox" checked={false} />
-                    <span className="checkmark"></span>
-                    </label>
-
-				</div>
-			</form>
-		</div>
-	);
+          <label className="container">
+            One
+            <input type="checkbox" checked={true} />
+            <span className="checkmark"></span>
+          </label>
+          <label className="container">
+            One
+            <input type="checkbox" checked={false} />
+            <span className="checkmark"></span>
+          </label>
+          <label className="container">
+            One
+            <input type="checkbox" checked={false} />
+            <span className="checkmark"></span>
+          </label>
+          <label className="container">
+            One
+            <input type="checkbox" checked={false} />
+            <span className="checkmark"></span>
+          </label>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default App;
