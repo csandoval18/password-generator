7295044320bad9660a6b224420dd5fa366238e11
csandoval <christiansandoval1018@outlook.com>
2023-05-23 16:19:40 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { FaRegCopy } from 'react-icons/fa';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App pg-app'>
+			<h1>Password Generator</h1>
+			<form action='' className='pg-form'>
+				<div className='input-wrapper'>
+					<input
+						type='text'
+						className='password'
+					/>
+					<div className='icon-wrapper'>
+						<FaRegCopy className='icon' />
+					</div>
+				</div>
+				<div className='settings-wrapper'>
+					<div className='row-space-between'>
+						<h3>Character Length</h3>
+						<span className='count'>0</span>
+					</div>
+
+					<div className='slider-length'>
+						<div className='circle'></div>
+					</div>
+
+                    <label className="container">One
+                    <input type="checkbox" checked={false} />
+                    <span className="checkmark"></span>
+                    </label>
+
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default App;
